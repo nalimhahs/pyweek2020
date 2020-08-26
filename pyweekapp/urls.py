2d82bfa3084a73ebc8abad729e0ab3c9ef4fe4a2
Restyled.io <commits@restyled.io>
2020-08-26 13:14:40 +0000

    Restyled by autopep8

--- pyweekapp/urls.py
+++ pyweekapp/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.Home,name="home"),
-    path('signup/',views.SignUp,name="signup"),
-    path('login/',views.Login,name="login"),
-    path('logout/',views.Logout,name="logout"),
-    path('sm/',views.StudyMaterial,name="studymaterial"),
-    path('events/',views.EventRegister,name="eventregister"),
-    path('confirm/',views.Confirm,name="confirm"),
-    path('klepsydra/<str:val>/',views.AdminData,name="data"),
-    path('guide/',views.Install,name="install"),
+    path('', views.Home, name="home"),
+    path('signup/', views.SignUp, name="signup"),
+    path('login/', views.Login, name="login"),
+    path('logout/', views.Logout, name="logout"),
+    path('sm/', views.StudyMaterial, name="studymaterial"),
+    path('events/', views.EventRegister, name="eventregister"),
+    path('confirm/', views.Confirm, name="confirm"),
+    path('klepsydra/<str:val>/', views.AdminData, name="data"),
+    path('guide/', views.Install, name="install"),
 ]
